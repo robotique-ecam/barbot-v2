627325feba3df391d9c50cff3e652aeb9a7b625b
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2019-09-14 13:37:14 +0200

    Added Dialog

--- gui/app.py
+++ gui/app.py
@@ -7,8 +7,9 @@
 import sys
 import time
 from PyQt5 import QtCore
-from ui import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from ui_main import Ui_MainWindow
+from ui_password import Ui_Dialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
@@ -27,17 +28,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+        self.password_dialog = PasswordDialog(self)
         self.drink1.clicked.connect(lambda: self.tabWidget.setCurrentIndex(1))
         self.drink1.clicked.connect(self.loading)
 
     def loading(self):
         """Progress bar function."""
         self.completed = 0
+        self.password_dialog.show()
         while self.completed < 100:
             self.completed += 0.00001
             self.progress.setValue(self.completed)
         time.sleep(2)
         self.tabWidget.setCurrentIndex(2)
+
+
+class PasswordDialog(QDialog, Ui_Dialog):
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        Ui_Dialog.__init__(self)
+        self.setupUi(self)
 
 
 if __name__ == "__main__":
