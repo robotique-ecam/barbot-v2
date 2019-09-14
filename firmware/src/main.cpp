8160db27b613b26ff6dc6eb0329197e729cb8f8c
3wnbr1 <ewen.brun@icloud.com>
2019-09-14 12:16:57 +0200

    Added firmware

---
+++ firmware/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <AccelStepper.h>
+
+
+#define dirPin 2
+#define stepPin 3
+#define motorInterfaceType 1
+
+
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+
+
+void setup() {
+  // Microsteps are set to 1/32
+  // Therefore 200 x 32 = 6 400 gives 
+  // Set the maximum speed in steps per second:
+  stepper.setMaxSpeed(6400);
+}
+
+void loop() {
+  // Set the speed in steps per second:
+  stepper.setSpeed(6400);
+  // Step the motor with a constant speed as set by setSpeed():
+  stepper.runSpeed();
+}
