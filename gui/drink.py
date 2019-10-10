4060c7e3da451f8243b8627d0919eb6ecd9078b7
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2019-10-10 20:35:49 +0200

    Added ;

--- gui/drink.py
+++ gui/drink.py
@@ -45,7 +45,7 @@ class DrinkDialog(QDialog, Ui_Dialog):
         self.parent().serial.write(("F" + str(self.number_button(self.parent().name_clicked)) + ";").encode())
 
     def pump_stop(self):
-        self.parent().serial.write("S".encode() + ";")
+        self.parent().serial.write("S;".encode())
 
     def purge_start(self):
         self.parent().serial.write(("R" + str(self.number_button(self.parent().name_clicked)) + ";").encode())
